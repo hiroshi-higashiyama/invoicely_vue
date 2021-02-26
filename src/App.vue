30a0799033adbf9cb691753597f330e87e8684ba
hiroshi <s20840011@gmail.com>
2021-02-26 22:48:11 +0900

    second

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,30 @@
             <router-link to="/dashboard" class="navbar-item"
               >Dashboard</router-link
             >
-          </template>
-
-          <template v-else>
-             <router-link to="/" class="navbar-item"
-              >Home</router-link
-            >
 
             <div class="navbar-item">
               <div class="buttons">
-                <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
-                <router-link to="/log-in" class="button is-light">Log in</router-link>
+                <router-link to="/dashboard/my-account" class="button is-light"
+                  >My account</router-link
+                >
               </div>
             </div>
           </template>
 
+          <template v-else>
+            <router-link to="/" class="navbar-item">Home</router-link>
+
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link to="/sign-up" class="button is-success"
+                  ><strong>Sign up</strong></router-link
+                >
+                <router-link to="/log-in" class="button is-light"
+                  >Log in</router-link
+                >
+              </div>
+            </div>
+          </template>
         </div>
       </div>
     </nav>
@@ -49,12 +58,12 @@ export default {
   beforeCreate() {
     this.$store.commit("initializeStore");
 
-    const token = this.$store.state.token
+    const token = this.$store.state.token;
 
     if (token) {
-      axios.defaults.headers.common['Authorization'] = "Token " + token
-      } else {
-        axios.defaults.headers.common['Authorization'] = ""
+      axios.defaults.headers.common["Authorization"] = "Token " + token;
+    } else {
+      axios.defaults.headers.common["Authorization"] = "";
     }
   },
 };
